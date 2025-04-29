c7679433c38104b284d235b9d56e9d3cf360f728
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-29 07:29:29 +0200

    init

--- app/todo/form/page.tsx
+++ app/todo/form/page.tsx
@@ -73,7 +73,7 @@ const GetTodoByIdForm = () => {
               <p><>Done:</> {todo.done ? "Yes" : "No"}</p>
               <p><>Details:</> {todo.details || "N/A"}</p>
               <Link href="/todo">
-                <button  className="flex items-center">
+                <button  className="flex items-center hover:text-zinc-600 p-2 mt-4 rounded">
                   <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
                   </svg>
