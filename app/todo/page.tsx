dc24016ce8b53cbb7c78cf2bf5c7722cd0d41beb
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-29 17:33:55 +0200

    dugmad na tabli

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -110,7 +110,7 @@ export default function TodoTable() {
                     />
                   </td>
                   <td>
-                    <div className="flex gap-2">
+                    <div className="flex gap-2 flex-row-reverse">
 
                       <Link href="/todo/form" onClick={() => setUser((todo.id))} >
                         <button className="px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600 transition">Pregled</button>
