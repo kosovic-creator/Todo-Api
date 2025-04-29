9df1959505e939f2b0287877a4d2e287537a080e
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-29 07:40:44 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 
-import Nav from "@/components/Nav";
-import { Home as HomeIcon } from "lucide-react";
+
 import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>
+  return <div className="flex flex-col   h-screen ml-2 text-blue-600 underline p-4">
     <Link href="/reducer/counter_reducer">CounterReducer</Link>
     <Link href="/reducer/slozeni_reducer">SlozeniReducer</Link>
     <Link href="/reducer/reducer_with_two_states">UdemiReducer</Link>
