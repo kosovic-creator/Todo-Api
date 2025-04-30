1fd116e58318e8748c12790181a89450b52159aa
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-30 05:38:12 +0200

    full

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,7 +8,7 @@ const Nav = () => {
   //console.log(session);
   return (
     <header className="bg-black text-white flex-shrink-0">
-      <nav className="flex justify-between items-center w-full px-10 py-4">
+      <nav className="flex justify-between items-center w-full max-w-7xl mx-auto p-4">
       <Link href="/"><HomeIcon/></Link>
         <div className="flex gap-10">
 
