56c726baff519ed3bb9889f266dd9bc0ed3a7bd6
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-30 19:56:50 +0200

    search

--- app/client-server/client-server-url/page.tsx
+++ app/client-server/client-server-url/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+'use client';
+import { useSearchParams } from 'next/navigation'
+import React from 'react';
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+interface SearchParams {
+  [key: string]: string | string[] | undefined;
+}
+
+export default function Page() {
+  const searchParams = useSearchParams();
+  const search = searchParams.get('kapacitet'); // npr. /dashboard?search=my-project
+  return <>Search: {search}</>;
 }
