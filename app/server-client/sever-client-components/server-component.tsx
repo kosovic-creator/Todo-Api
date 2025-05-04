66577e4dae6fd7e6209dc061375d404eaf5b50f4
kosovic-creator <drasko.kosovic@icloud.com>
2025-05-04 06:50:00 +0200

    prisma

--- app/server-client/sever-client-components/server-component.tsx
+++ app/server-client/sever-client-components/server-component.tsx
@@ -4,7 +4,7 @@ export default async function ServerComponent() {
 
     const data = await fetch('https://jsonplaceholder.typicode.com/posts/1')
     const json = await data.json()
-    console.log(json)
+    // console.log(json)
 
 
     return (
